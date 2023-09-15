aa335ce18a23e730b1aede654a07b897b5abd9d3
Robert Smith <130311054+rsmith1631@users.noreply.github.com>
2023-09-15 15:53:22 -0700

    Made changes to the top paragraph and also added margin to the contact form

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['C# Developer', 'JavaScript Engineer', 'Full-Stack Extraordinaire!'],
+    strings: ['C# Developer!', 'JavaScript Engineer!', 'Full-Stack Extraordinaire!'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
